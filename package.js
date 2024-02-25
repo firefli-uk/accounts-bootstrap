255ece073d5dc643a1ee837b983b81682c15073b
Matt Mills <github@firefli.co.uk>
2024-02-25 21:15:50 +0000

    update to use accounts-core
    Issue with template helper AT.prototype.atPwdFormHelpers returning non-object form core

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: 'Accounts Templates styled for Twitter Bootstrap.',
-  version: '1.14.2',
-  name: 'useraccounts:bootstrap',
-  git: 'https://github.com/meteor-useraccounts/bootstrap.git',
+  version: '1.15.1',
+  name: 'firefli-uk:accounts-bootstrap',
+  git: 'https://github.com/firefli-uk/accounts-bootstrap.git',
 });
 
-Package.on_use(function(api, where) {
-  api.versionsFrom('METEOR@1.0');
+Package.onUse(function(api, where) {
+  api.versionsFrom('METEOR@2.4');
 
   api.use([
     'templating',
@@ -14,18 +14,18 @@ Package.on_use(function(api, where) {
   ], 'client');
 
   api.use([
-    'useraccounts:core',
+    'firefli-uk:accounts-core',
   ], ['client', 'server']);
 
   // Requires all routing packages loads before this asking for weak dependencies.
-  api.use('useraccounts:flow-routing@1.14.2', ['client', 'server'], {weak: true});
+  api.use('useraccounts:flow-routing@1.15.0', ['client', 'server'], {weak: true});
   api.use('useraccounts:iron-routing@1.14.2', ['client', 'server'], {weak: true});
 
   api.imply([
-    'useraccounts:core@1.14.2',
+    'firefli-uk:accounts-core',
   ], ['client', 'server']);
 
-  api.add_files([
+  api.addFiles([
     'lib/at_error.html',
     'lib/at_error.js',
     'lib/at_form.html',
@@ -67,10 +67,10 @@ Package.on_use(function(api, where) {
   ], ['client']);
 });
 
-Package.on_test(function(api) {
+Package.onTest(function(api) {
   api.use([
     'useraccounts:bootstrap',
-    'useraccounts:core@1.14.2',
+    'useraccounts:core@1.16.2',
   ]);
 
   api.use([
@@ -79,7 +79,7 @@ Package.on_test(function(api) {
     'test-helpers'
   ], ['client', 'server']);
 
-  api.add_files([
+  api.addFiles([
     'tests/tests.js'
   ], ['client', 'server']);
 });
